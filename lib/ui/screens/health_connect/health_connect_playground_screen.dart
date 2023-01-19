96a4ef918480c13d6da045dbfe889bc01a3557f9
dnieln7 <dnieln7@gmail.com>
2023-01-19 15:10:23 -0600

    Implemented transmission package

--- lib/ui/screens/health_connect/health_connect_playground_screen.dart
+++ lib/ui/screens/health_connect/health_connect_playground_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rook_extraction_demo/transmission_secrets.dart';
 import 'package:rook_extraction_demo/ui/widgets/predefined.dart';
 import 'package:rook_extraction_demo/ui/widgets/scrollable_scaffold.dart';
 import 'package:rook_extraction_demo/ui/widgets/section_title.dart';
 import 'package:rook_health_connect/rook_health_connect.dart';
+import 'package:rook_transmission/rook_transmission.dart';
 
 const String healthConnectPlaygroundScreenRoute = '/health-connect/playground';
 
@@ -27,13 +29,29 @@ class HealthConnectPlaygroundScreen extends StatefulWidget {
 
 class _HealthConnectPlaygroundScreenState
     extends State<HealthConnectPlaygroundScreen> {
+
+  final RookTransmissionManager manager = RookTransmissionManager(
+    TransmissionSecrets.url,
+    'daniel.nolasco@rookmotion.com',
+    TransmissionSecrets.clientUUID,
+    TransmissionSecrets.clientPassword,
+  );
+
   DateTime oldestDate = DateTime.now().subtract(const Duration(days: 29));
   DateTime soonestDate = DateTime.now().subtract(const Duration(days: 1));
 
-  DateTime sleepSummaryDate = DateTime.now().subtract(const Duration(days: 1));
-  bool sleepFetching = false;
-  SleepSummary? sleepSummary;
-  String? sleepError;
+  DateTime sleepDate = DateTime.now().subtract(const Duration(days: 1));
+  bool sleepExtracting = false;
+  SleepSummary? sleepExtracted;
+  String? sleepExtractError;
+
+  bool sleepEnqueueing = false;
+  bool? sleepEnqueued;
+  String? sleepSEnqueueError;
+
+  bool syncing = false;
+  bool? synced;
+  String? syncError;
 
   @override
   void initState() {
@@ -49,25 +67,66 @@ class _HealthConnectPlaygroundScreenState
       alignment: Alignment.topCenter,
       child: Column(
         children: [
-          const SectionTitle('Sleep'),
-          ElevatedButton(
-            onPressed: sleepFetching ? null : showSleepSelector,
-            child: const Text('Select a date'),
+          ...hcData<SleepSummary>(
+            name: 'Sleep',
+            extract: showSleepSelector,
+            extracting: sleepExtracting,
+            extracted: sleepExtracted,
+            extractError: sleepExtractError,
+            enqueue: enqueueSleep,
+            enqueueing: sleepEnqueueing,
+            enqueued: sleepEnqueued,
+            enqueueError: sleepSEnqueueError,
           ),
-          const SizedBox(height: 10),
-          if (sleepSummary != null) Text('Sleep summary: $sleepSummary'),
-          if (sleepError != null) Text('Error: $sleepError'),
-          sectionDivider,
+          const SectionTitle('Sync'),
+          ElevatedButton(
+            onPressed: syncing ? null : syncAll,
+            child: const Text('Sync all'),
+          ),
+          if (synced == true) const Text('All synced!'),
+          if (synced == false) Text('Error: $syncError'),
         ],
       ),
     );
   }
 
+  List<Widget> hcData<D>({
+    required String name,
+    required Function() extract,
+    required bool extracting,
+    D? extracted,
+    String? extractError,
+    required Function() enqueue,
+    required bool enqueueing,
+    bool? enqueued,
+    String? enqueueError,
+  }) {
+    return [
+      SectionTitle(name),
+      ElevatedButton(
+        onPressed: extracting ? null : extract,
+        child: const Text('Select a date'),
+      ),
+      const SizedBox(height: 10),
+      if (extracted != null) Text('$name: $extracted'),
+      if (extractError != null) Text('Error: $extractError'),
+      const SizedBox(height: 10),
+      ElevatedButton(
+        onPressed: (enqueueing || extracted == null) ? null : enqueue,
+        child: const Text('Enqueue this'),
+      ),
+      if (enqueued == true) Text('$name enqueued!'),
+      if (enqueued == false) Text('Error: $enqueueError'),
+      sectionDivider,
+    ];
+  }
+
   void getLastDate() async {
     try {
-      final sleepSummaryLastDate = await widget.args.manager.getSleepSummaryLastDate();
+      final sleepSummaryLastDate =
+          await widget.args.manager.getSleepSummaryLastDate();
 
-      setState(() => sleepSummaryDate = sleepSummaryLastDate);
+      setState(() => sleepDate = sleepSummaryLastDate);
     } catch (ignored) {
       // Ignored
     }
@@ -76,7 +135,7 @@ class _HealthConnectPlaygroundScreenState
   void showSleepSelector() {
     showDatePicker(
       context: context,
-      initialDate: sleepSummaryDate,
+      initialDate: sleepDate,
       firstDate: oldestDate,
       lastDate: soonestDate,
     ).then((selected) => getSleep(selected));
@@ -85,26 +144,89 @@ class _HealthConnectPlaygroundScreenState
   void getSleep(DateTime? selectedDate) async {
     if (selectedDate != null) {
       setState(() {
-        sleepFetching = true;
-        sleepSummary = null;
-        sleepError = null;
+        sleepExtracting = true;
+        sleepExtracted = null;
+        sleepExtractError = null;
       });
 
       try {
         final summary = await widget.args.manager.getSleepSummary(selectedDate);
 
         setState(() {
-          sleepFetching = false;
-          sleepSummary = summary;
-          sleepError = null;
+          sleepExtracting = false;
+          sleepExtracted = summary;
+          sleepExtractError = null;
         });
       } catch (error) {
         setState(() {
-          sleepFetching = false;
-          sleepSummary = null;
-          sleepError = '$error';
+          sleepExtracting = false;
+          sleepExtracted = null;
+          sleepExtractError = '$error';
         });
       }
+    }
+  }
+
+  void enqueueSleep() async {
+    if (sleepExtracted != null) {
+      setState(() {
+        sleepEnqueueing = true;
+        sleepEnqueued = null;
+        sleepSEnqueueError = null;
+      });
+
+      try {
+        final item = SleepSummaryItem(
+          sourceOfData: 'Health Connect',
+          sleepStartDatetime: DateTimeFormatter.fromIso8601ToRook(
+            sleepExtracted!.sleepStartDatetime,
+          ),
+          sleepEndDatetime: DateTimeFormatter.fromIso8601ToRook(
+            sleepExtracted!.sleepEndDatetime,
+          ),
+          sleepDate: sleepExtracted!.sleepDate,
+          sleepDurationSeconds: sleepExtracted!.sleepDurationSeconds,
+          timeInBedSeconds: sleepExtracted!.timeInBedSeconds,
+        );
+
+        await manager.enqueueSleepSummary(item);
+
+        setState(() {
+          sleepEnqueueing = false;
+          sleepEnqueued = true;
+          sleepSEnqueueError = null;
+        });
+      } catch (error) {
+        setState(() {
+          sleepEnqueueing = false;
+          sleepEnqueued = false;
+          sleepSEnqueueError = '$error';
+        });
+      }
+    }
+  }
+
+  void syncAll() async {
+    setState(() {
+      syncing = true;
+      synced = null;
+      syncError = null;
+    });
+
+    try {
+      await manager.syncAll();
+
+      setState(() {
+        syncing = false;
+        synced = true;
+        syncError = null;
+      });
+    } catch (error) {
+      setState(() {
+        syncing = false;
+        synced = false;
+        syncError = '$error';
+      });
     }
   }
 }
